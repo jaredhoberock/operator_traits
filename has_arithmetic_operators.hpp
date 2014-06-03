d8a040a5e5fcef99e9af0c71dc5c6c9d6ef8ff39
Jared Hoberock <jaredhoberock@gmail.com>
2014-06-03 14:38:35 -0700

    Default to common_type<T1,T2> for the result type

--- has_arithmetic_operators.hpp
+++ has_arithmetic_operators.hpp
@@ -29,7 +29,7 @@ no operator/(any, any);
 no operator/=(any, any);
 
 
-template<typename T1, typename T2 = T1, typename Result = T1>
+template<typename T1, typename T2 = T1, typename Result = typename std::common_type<T1,T2>::type>
 struct has_operator_plus
   : std::is_same<
       decltype(std::declval<T1>() + std::declval<T2>()),
@@ -49,7 +49,7 @@ struct has_operator_plus_assign
 };
 
 
-template<typename T1, typename T2 = T1, typename Result = T1>
+template<typename T1, typename T2 = T1, typename Result = typename std::common_type<T1,T2>::type>
 struct has_operator_minus
   : std::is_same<
       decltype(std::declval<T1>() - std::declval<T2>()),
@@ -69,7 +69,7 @@ struct has_operator_minus_assign
 };
 
 
-template<typename T1, typename T2 = T1, typename Result = T1>
+template<typename T1, typename T2 = T1, typename Result = typename std::common_type<T1,T2>::type>
 struct has_operator_multiplies
   : std::is_same<
       decltype(std::declval<T1>() * std::declval<T2>()),
@@ -89,7 +89,7 @@ struct has_operator_multiplies_assign
 };
 
 
-template<typename T1, typename T2 = T1, typename Result = T1>
+template<typename T1, typename T2 = T1, typename Result = typename std::common_type<T1,T2>::type>
 struct has_operator_divides
   : std::is_same<
       decltype(std::declval<T1>() / std::declval<T2>()),
@@ -112,7 +112,7 @@ struct has_operator_divides_assign
 }
 
 
-template<typename T1, typename T2 = T1, typename Result = T1>
+template<typename T1, typename T2 = T1, typename Result = typename std::common_type<T1,T2>::type>
 struct has_operator_plus : __has_arithmetic_namespace::has_operator_plus<T1,T2,Result> {};
 
 
@@ -120,7 +120,7 @@ template<typename T1, typename T2 = T1, typename Result = T1&>
 struct has_operator_plus_assign : __has_arithmetic_namespace::has_operator_plus_assign<T1,T2,Result> {};
 
 
-template<typename T1, typename T2 = T1, typename Result = T1>
+template<typename T1, typename T2 = T1, typename Result = typename std::common_type<T1,T2>::type>
 struct has_operator_minus : __has_arithmetic_namespace::has_operator_minus<T1,T2,Result> {};
 
 
@@ -128,7 +128,7 @@ template<typename T1, typename T2 = T1, typename Result = T1&>
 struct has_operator_minus_assign : __has_arithmetic_namespace::has_operator_minus_assign<T1,T2,Result> {};
 
 
-template<typename T1, typename T2 = T1, typename Result = T1>
+template<typename T1, typename T2 = T1, typename Result = typename std::common_type<T1,T2>::type>
 struct has_operator_multiplies : __has_arithmetic_namespace::has_operator_multiplies<T1,T2,Result> {};
 
 
@@ -136,7 +136,7 @@ template<typename T1, typename T2 = T1, typename Result = T1&>
 struct has_operator_multiplies_assign : __has_arithmetic_namespace::has_operator_multiplies_assign<T1,T2,Result> {};
 
 
-template<typename T1, typename T2 = T1, typename Result = T1>
+template<typename T1, typename T2 = T1, typename Result = typename std::common_type<T1,T2>::type>
 struct has_operator_divides : __has_arithmetic_namespace::has_operator_divides<T1,T2,Result> {};
 
 
